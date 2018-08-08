55201ecc5cba0fa962e558e8528bdbcd2bbb9c94
Raphael Krausz <krauszraphael@gmail.com>
2018-08-08 17:38:25 +0200

    Added in colours for chart

--- ClientApp/components/history/history.ts
+++ ClientApp/components/history/history.ts
@@ -1,12 +1,33 @@
 import * as ko from 'knockout';
 
+
+interface CurrencyRate {
+    date: string;
+    currency: number;
+    rate: number;
+}
+
+
+
 class HistoryViewModel {
     public currentCount = ko.observable(0);
+
 
     public incrementCounter() {
         let prevCount = this.currentCount();
         this.currentCount(prevCount + 1);
     }
+
+    public latestRates = ko.observableArray<CurrencyRate>();
+
+    constructor() {
+        fetch('api/Currency/LatestRates')
+            .then(response => response.json() as Promise<CurrencyRate[]>)
+            .then(data => {
+                this.latestRates(data);
+            });
+    }
+
 }
 
 export default { viewModel: HistoryViewModel,
