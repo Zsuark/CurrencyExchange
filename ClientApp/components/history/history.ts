ca494babb5ee11278059212cc4540723b9d9265f
Raphael Krausz <krauszraphael@gmail.com>
2018-08-11 00:00:51 +0200

    Rickshaw Graphs now work. TODO: Setup npm to use private repo rather than locally modified type definition

--- ClientApp/components/history/history.ts
+++ ClientApp/components/history/history.ts
@@ -1,22 +1,59 @@
 import * as ko from 'knockout';
+import * as Rickshaw from 'rickshaw';
+
+/*
+These helped!
+
+ - https://stackoverflow.com/questions/49267769/typescript-index-d-ts-declaration-for-deeply-nested-function-in-an-npm-packagefdsa
 
 
-interface CurrencyRate {
-    date: string;
-    currency: number;
-    rate: number;
-}
-
+*/
 
 
 class HistoryViewModel {
     public currentCount = ko.observable(0);
+
+    private latestRates = ko.observableArray<CurrencyRate>();
+
 
 
     public incrementCounter() {
         let prevCount = this.currentCount();
         this.currentCount(prevCount + 1);
     }
+
+    private getLatestRates() {
+        fetch('api/Currency/LatestRates')
+            .then(response => response.json() as Promise<CurrencyRate[]>)
+            .then(data => {
+                this.latestRates(data);
+            });
+    }
+
+    constructor() {
+        var chart = new Rickshaw.Graph( {
+            element: document.querySelector("#chart"), 
+            width: 500, 
+            height: 200, 
+            series: [{
+                color: 'steelblue',
+                data: [ 
+                    { x: 0, y: 40 }, 
+                    { x: 1, y: 49 }, 
+                    { x: 2, y: 38 }, 
+                    { x: 3, y: 30 }, 
+                    { x: 4, y: 32 } ]
+            }]
+        });
+    
+        chart.render();
+
+    }
+
+}
+
+
+    /*
 
     public latestRates = ko.observableArray<CurrencyRate>();
 
@@ -26,9 +63,12 @@ class HistoryViewModel {
             .then(data => {
                 this.latestRates(data);
             });
-    }
+            }
 
-}
+*/
+
 
 export default { viewModel: HistoryViewModel,
-                 template: require('./history.html') };
+                 template: require('./history.html') }; /*,
+                 afterRender:   HistoryViewModel.drawChart }; */
+
