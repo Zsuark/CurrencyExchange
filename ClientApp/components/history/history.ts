f53c2428f3a8e09942f039a14d421f5c9cdc9da3
Raphael Krausz <krauszraphael@gmail.com>
2018-08-14 11:42:39 +0200

    plottable may not work so well.... :/

--- ClientApp/components/history/history.ts
+++ ClientApp/components/history/history.ts
@@ -1,11 +1,45 @@
 import * as ko from 'knockout';
+import * as Plottable from 'plottable';
+
+alert("Before");
+
 
 class HistoryViewModel {
-    public currentCount = ko.observable(0);
 
-    public incrementCounter() {
-        let prevCount = this.currentCount();
-        this.currentCount(prevCount + 1);
+    constructor() {
+
+      let xScale = new Plottable.Scales.Linear();
+      let yScale = new Plottable.Scales.Linear();
+    
+      let xAxis = new Plottable.Axes.Numeric(xScale, "bottom");
+      let yAxis = new Plottable.Axes.Numeric(yScale, "left");
+    
+      let plot = new Plottable.Plots.Line();
+      plot.x(function(d) { return d.x; }, xScale);
+      plot.y(function(d) { return d.y; }, yScale);
+    
+      let data = [
+        { "x": 0, "y": 1 },
+        { "x": 1, "y": 2 },
+        { "x": 2, "y": 4 },
+        { "x": 3, "y": 8 }
+      ];
+    
+      let dataset = new Plottable.Dataset(data);
+      plot.addDataset(dataset);
+    
+      let chart = new Plottable.Components.Table([
+        [yAxis, plot],
+        [null, xAxis]
+      ]);
+
+        alert("div#tutorial-result: " + JSON.stringify(document.querySelector("div#tutorial-result")));
+
+        chart.renderTo("div#tutorial-result");
+
+
+        alert("Here3");
+
     }
 }
 
